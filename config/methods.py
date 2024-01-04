4d2e134a278e4b72afe84be0d6c0f60db5c9614f
MrChriwo <96289753+MrChriwo@users.noreply.github.com>
2024-01-04 18:01:52 +0100

    hotfix for methods

--- config/methods.py
+++ config/methods.py
@@ -3,6 +3,7 @@ import json
 import subprocess
 
 # retrieve the ascii art / logo
+
 def get_ascii_art() -> str:
     try:
         file = os.path.join(os.path.dirname(__file__), 'ascii_art.txt')
@@ -16,7 +17,7 @@ def get_ascii_art() -> str:
         print(f"An error occurred: {e}")
 
 
-# retrieve the version
+
 def get_version() -> str:
     try:
         file = os.path.join(os.path.dirname(__file__), 'version.txt')
@@ -24,6 +25,21 @@ def get_version() -> str:
         with open(file, "r") as file:
             version = file.read()
             return version
+    except FileNotFoundError:
+        print(f"File not found: {file}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+# retrieve the config file
+def get_config() -> dict:
+    try:
+        file = os.path.join(os.path.dirname(__file__), 'config.json')
+
+        with open(file, "r") as f:
+            config = json.load(f)
+            return config
     except FileNotFoundError:
         print(f"File not found: {file}")
     except Exception as e:
@@ -63,31 +79,7 @@ def display_home() -> None:
 
 
 
-# create videos from all scenes
-def create_videos(quality= "medium") -> None:
-    if quality == "low":
-        quality = "-ql"
-    elif quality == "medium":
-        quality = "-qm"
-    elif quality == "high":
-        quality = "-qh"
-    elif quality == "ultra": # 4k rendering 
-        quality = "-qk"
-
-    try:
-        # get all pyscripts from the anim folder(all scenes actually)
-        files = os.listdir(os.path.join(os.path.dirname(__file__), "..", "anim"))
-        files = [file for file in files if file.endswith(".py")]
-        
-        # compile all scenes
-        for file in files:
-            subprocess.run(["manim", {quality}, file, "-a"], cwd=os.path.join(os.path.dirname(__file__), "..", "anim"))
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-# helping function for concat_videos
+# helping function for concat_video
 def get_all_videos(): 
     cwd = os.getcwd()
     anim_video_folder = os.path.join(cwd, "anim", "media", "videos")
@@ -102,24 +94,34 @@ def get_all_videos():
                     mp4_files.append(file)
     return mp4_files
 
+
+
+def create_video() -> None:
+    try:
+        # get all pyscripts from the anim folder
+        files = os.listdir(os.path.join(os.path.dirname(__file__), "..", "anim"))
+        files = [file for file in files if file.endswith(".py")]
+        
+        # compile all the files
+        for file in files:
+            subprocess.run(["manim", "-ql", file, "-a"], cwd=os.path.join(os.path.dirname(__file__), "..", "anim"))
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
     
-# concatenate all scenes together    
+# concatenate the videos 
 def concat_videos () -> None:
     try:
-        files = get_all_videos() # all mp4 files 
+        files = get_all_videos()
         anim_folder = os.path.join(os.path.dirname(__file__), "..", "anim")
         tmp_compile_file = os.path.join(anim_folder, "tmp.txt")
 
         with open(tmp_compile_file, "w") as f:
             for file in files:
                 f.write(f"file '{file}'\n")
-
-        # concatenate all videos using ffmpeg
+        # run the ffmpeg command
         subprocess.run(["ffmpeg", "-f", "concat", "-safe", "0", "-i", tmp_compile_file, "-c", "copy", "output.mp4"], cwd=os.path.join(os.path.dirname(__file__), "..", "anim"))
-        
-        # remove the tmp file
         os.remove(tmp_compile_file)
-
     except Exception as e:
-        print(f"An error occurred while concatenating the videos: {e}") 
+        print(f"An error occurred: {e}") 
 
