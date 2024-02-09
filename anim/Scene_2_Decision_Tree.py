f2d3742b025c715ae2341617f9fdeac6d02c9b96
Christopher Wolf <christopher-wolf@outlook.de>
2024-02-09 02:26:40 +0100

    tree and outro

--- anim/Scene_2_Decision_Tree.py
+++ anim/Scene_2_Decision_Tree.py
@@ -220,83 +220,105 @@ class b_explain_vocabs(Scene):
 
 class c(Scene):
     
-        def create_tree(self): 
-            G = nx.Graph()
+    def create_tree(self): 
+        G = nx.Graph()
 
-            G.add_node("ROOT")
+        G.add_node("ROOT")
 
-            for i in range(5):
-                G.add_node("Child_%i" % i)
-                G.add_node("Grandchild_%i" % i)
+        for i in range(5):
+            G.add_node("Child_%i" % i)
+            G.add_node("Grandchild_%i" % i)
 
-                G.add_edge("ROOT", "Child_%i" % i)
-                G.add_edge("Child_%i" % i, "Grandchild_%i" % i)
+            G.add_edge("ROOT", "Child_%i" % i)
+            G.add_edge("Child_%i" % i, "Grandchild_%i" % i)
 
-                # all nodes black
-                G.nodes["Child_%i" % i]["color"] = "black"
+            # all nodes black
+            G.nodes["Child_%i" % i]["color"] = "black"
 
 
-            return (Graph(list(G.nodes), list(G.edges), layout="tree", root_vertex="ROOT").set_color_by_gradient(BLUE, GREEN, RED))
-        
-        def construct(self):
-            title = (Text("Decision Trees", font="Montserrat", color=WHITE).scale(1.5))
-            title.to_edge(UP)
-            title.set_color_by_gradient(BLUE, GREEN, RED)
-            self.play(Write(title))
-            tree_classifier = self.create_tree()
-            tree_regressor = self.create_tree()
-            tree_classifier.scale(0.3)
-            tree_regressor.scale(0.3)
-            tree_classifier.next_to(LEFT*4.5, buff=0.5)
-            tree_regressor.next_to(RIGHT*2, buff=0.5)
+        return (Graph(list(G.nodes), list(G.edges), layout="tree", root_vertex="ROOT").set_color_by_gradient(BLUE, GREEN, RED))
+    
+    def construct(self):
+        title = (Text("Decision Trees", font="Montserrat", color=WHITE).scale(1.5))
+        title.to_edge(UP)
+        title.set_color_by_gradient(BLUE, GREEN, RED)
+        self.play(Write(title))
+        tree_classifier = self.create_tree()
+        tree_regressor = self.create_tree()
+        tree_classifier.scale(0.3)
+        tree_regressor.scale(0.3)
+        tree_classifier.next_to(LEFT*4.5, buff=0.5)
+        tree_regressor.next_to(RIGHT*2, buff=0.5)
 
-            title_classifier = Text("Classification Tree", font="Montserrat", color=WHITE).scale(0.7)
-            title_regressor = Text("Regression Tree", font="Montserrat", color=WHITE).scale(0.7)
-            title_classifier.next_to(tree_classifier, DOWN, buff=0.3)
-            title_regressor.next_to(tree_regressor, DOWN, buff=0.3)
+        title_classifier = Text("Classification Tree", font="Montserrat", color=WHITE).scale(0.7)
+        title_regressor = Text("Regression Tree", font="Montserrat", color=WHITE).scale(0.7)
+        title_classifier.next_to(tree_classifier, DOWN, buff=0.3)
+        title_regressor.next_to(tree_regressor, DOWN, buff=0.3)
 
-            self.play(Create(tree_classifier))
-            self.wait(1)
-            self.play(Write(title_classifier))
-            self.wait(1)
+        self.play(Create(tree_classifier))
+        self.wait(1)
+        self.play(Write(title_classifier))
+        self.wait(1)
 
-            self.play(Create(tree_regressor))
-            self.wait
-            self.play(Write(title_regressor))
-            self.wait(1)
+        self.play(Create(tree_regressor))
+        self.wait
+        self.play(Write(title_regressor))
+        self.wait(1)
 
-            # self.play(title_classifier.scale, 1.5, title_classifier.shift, UP*0.8)
-            title_classifier.animate.shift( RIGHT*0.8)
+        # self.play(title_classifier.scale, 1.5, title_classifier.shift, UP*0.8)
+        title_classifier.animate.shift( RIGHT*0.8)
 
-            explain_classifier = Text("Used for classification tasks", font="Montserrat", color=WHITE).scale(0.4)
-            explain_classifier.next_to(title_classifier, DOWN, buff=0.3)
-            self.play(Write(explain_classifier))
+        explain_classifier = Text("Used for classification tasks", font="Montserrat", color=WHITE).scale(0.4)
+        explain_classifier.next_to(title_classifier, DOWN, buff=0.3)
+        self.play(Write(explain_classifier))
 
-            explain_classifier_2 = Text("Predicts a class", font="Montserrat", color=WHITE).scale(0.4)
-            self.wait(1)
-            explain_classifier_2.next_to(explain_classifier, DOWN, buff=0.3)
-            self.wait(1)
-            self.play(Write(explain_classifier_2))
+        explain_classifier_2 = Text("Predicts a class", font="Montserrat", color=WHITE).scale(0.4)
+        self.wait(1)
+        explain_classifier_2.next_to(explain_classifier, DOWN, buff=0.3)
+        self.wait(1)
+        self.play(Write(explain_classifier_2))
 
-            explain_regressor = Text("Used for regression tasks", font="Montserrat", color=WHITE).scale(0.4)
-            explain_regressor.next_to(title_regressor, DOWN, buff=0.3)
-            explain_regressor_2 = Text("Predicts a numeric value", font="Montserrat", color=WHITE).scale(0.4)
-            explain_regressor_2.next_to(explain_regressor, DOWN, buff=0.3)
-            self.wait(1)
-            self.play(Write(explain_regressor))
-            self.wait(1)
-            self.play(Write(explain_regressor_2))
-            self.wait(2)
+        explain_regressor = Text("Used for regression tasks", font="Montserrat", color=WHITE).scale(0.4)
+        explain_regressor.next_to(title_regressor, DOWN, buff=0.3)
+        explain_regressor_2 = Text("Predicts a numeric value", font="Montserrat", color=WHITE).scale(0.4)
+        explain_regressor_2.next_to(explain_regressor, DOWN, buff=0.3)
+        self.wait(1)
+        self.play(Write(explain_regressor))
+        self.wait(1)
+        self.play(Write(explain_regressor_2))
+        self.wait(2)
 
-            self.play(FadeOut(*self.mobjects))
-            self.wait(0.3)
+        self.play(FadeOut(*self.mobjects))
+        self.wait(0.3)
 
 
 
 class d(Scene):
-    # scene that renders a tree
-    # node has text "it is an apple"
-    # then the decision notes are added
+
+    def __init__(self): 
+        super().__init__()
+        self.treeDepth = 1
+        self.treeChildren = 2
+        self.treeLayoutConfig = {"vertex_spacing": (1.8, 2.1)}
+        self.treeVertexConfig = {"radius": 0.85, "color": GREY, "fill_opacity": 1, "stroke_width": 3}
+        
+    def expand_vertex(self, g, vertex_id: str, depth: int):
+        new_vertices = [f"{vertex_id}/{i}" for i in range(self.treeChildren)]
+        new_edges = [(vertex_id, child_id) for child_id in new_vertices]
+        
+        g.add_edges(
+            *new_edges,
+            vertex_config=self.treeVertexConfig,
+            positions={
+                k: g.vertices[vertex_id].get_center() + 0.1 * DOWN for k in new_vertices
+            },
+        )
+        if depth < self.treeDepth:
+            for child_id in new_vertices:
+                self.expand_vertex(g, child_id, depth + 1)
+
+        return g
+
 
     def create_random_row(self):
         num = 0
@@ -304,8 +326,8 @@ class d(Scene):
             # random values for the columns
             random_row = [
                 np.random.choice(["rot", "orange"]),
-                np.random.randint(100, 200),
-                np.random.randint(5, 10),
+                np.random.choice(["leicht", "schwer"]),
+                np.random.choice(["klein", "groß"]),
                 np.random.choice(["apfel", "orange"]),
             ]
 
@@ -313,6 +335,9 @@ class d(Scene):
                 return [str(x) for x in random_row]
             else: 
                 num += 1
+
+    def calculate_gini(self, p0, p1):
+        return 1 - (p0 ** 2 + p1 ** 2)
 
 
     def construct(self):
@@ -322,23 +347,48 @@ class d(Scene):
         self.play(Write(title))
         self.wait(1)
 
-        # draw a table with the text "It is an apple"
-        # columns are "Farbe", "Gewicht", "Größe", "Obst"
-        # random values for the columns
+
+        rect = Rectangle(height=2, width=7, color=WHITE)
+        rect.set_color_by_gradient(BLUE, GREEN, RED)
+        rect.next_to(title, DOWN, buff=1.2)
+        self.play(Create(rect))
+        self.wait(1)
+
+        # wirte gini formula in the rectangle
+        gini_formula = MathTex("Gini", "(", "t", ")", "=", "1", "-", "(", "p", "_", "0", "^2", "+", "p", "_", "1", "^2", ")")
+        gini_formula.scale(1.3)
+        gini_formula.next_to(title, DOWN, buff=1.9)
+
+        gini_grp = VGroup(rect, gini_formula)
+        # display the gini formula grp 
+        self.play(Write(gini_grp))
+    
+        self.wait(2)
+
+        # scale grp down and  move the grp to left bottom corner 
+        self.play(gini_grp.animate.scale(0.4))
+        self.play(gini_grp.animate.shift(DOWN*3.2))
+        self.play(gini_grp.animate.shift(RIGHT*3.4))
+        self.wait(1)
 
         num_rows = 5
 
         table = Table(
             [["Farbe", "Gewicht", "Größe", "Obst"]] +
-            [self.create_random_row() for _ in range(num_rows)],
+            [["rot", "leicht", "klein", "apfel"],
+            ["orange", "schwer", "groß", "orange"],
+            ["rot", "leicht", "groß", "apfel"],
+            ["orange", "leicht", "klein", "apfel"],
+            ["rot", "schwer", "klein", "orange"]],
             h_buff=1,
             v_buff=0.5,
             line_config={"color": WHITE},
             fill_color=BLUE,
             fill_opacity=1,
         )
+        tree = Graph(["ROOT"], [], vertex_config=self.treeVertexConfig)
 
-        rows = ["Farbe", "Gewicht", "Größe", "Obst"]
+        rows = ["Farbe", "Gewicht", "Groesse", "Obst"]
 
         table.scale(0.7)
         table.next_to(title, DOWN, buff=0.5)
@@ -350,57 +400,173 @@ class d(Scene):
         self.play(table.animate.shift(LEFT*3))
         self.wait(1)
 
-        table[0][0].set_color(GREEN)
         self.wait(2)
 
-        # create a tree graph with root "Farbe" and a split for "rot" and "grün"
-        # Create a tree graph with root "Farbe" and a split for "rot" and "grün"
-        g = Graph(["Farbe", "rot", "orange", "Apfel", "Orange"], 
-                [("Farbe", "rot"), ("Farbe", "orange"), ("rot", "Apfel"), ("orange", "Orange")], 
-                root_vertex="Farbe", layout="tree", labels=True)
-        g.scale(0.5)
-        g.next_to(table, RIGHT, buff=1.2)
-        self.play(Create(g))
+        # column_texts = [Text(f"score {i+1}", font_size=24).next_to(table.get_columns()[i], DOWN) for i in range(4)]
+        scores = {any: any}
+
+        for i in range(3):
+            # Highlight column
+            self.play(table.get_columns()[i].animate.set_color(YELLOW))
+
+            # tree root node is the current column
+            tree = Graph([rows[i]], [], vertex_config=self.treeVertexConfig, labels=True)
+
+            tree = self.expand_vertex(tree, rows[i], 1)
+            tree = self.expand_vertex(tree, f"{rows[i]}/0", 2)
+            tree = self.expand_vertex(tree, f"{rows[i]}/1", 2)
+
+            tree.scale(0.6) 
+
+            self.add(tree)
+        
+            self.play(
+                tree.animate.change_layout(
+                    "tree",
+                    root_vertex=f"{rows[i]}",
+                    layout_config=self.treeLayoutConfig,
+                    
+                )
+            )
+
+
+            self.play(tree.animate.scale(0.7))
+            self.play(tree.animate.shift(RIGHT*3.7))
+            # animate a arrow pointing on the table fro right 
+            arrow = Arrow(RIGHT, LEFT*2, color=WHITE).scale(0.8)
+            arrow.next_to(table, RIGHT, buff=0.5)
+            arrow.shift(UP*0.9)
+            self.play(Create(arrow))
+
+        
+            apfel = 0
+            orange = 0
+            
+            for i in range(4):
+                if i % 2 == 0: 
+                    apfel += 1
+
+                    self.play(arrow.animate.set_color(RED), arrow.animate.shift(DOWN*0.6) )
+                else: 
+                    orange += 1
+                    self.play(arrow.animate.set_color(ORANGE), arrow.animate.shift(DOWN*0.6), arrow.animate.shift(DOWN*0.6) )
+                self.wait(1)
+
+
+            self.play(FadeOut(arrow))
+            self.wait(1)
+            self.play(table.get_columns()[i].animate.set_color(WHITE))
+
+            entities = table.get_rows()[1:]
+            num_apples = sum([1 for entity in entities if entity[i] == "apfel"])
+            num_oranges = sum([1 for entity in entities if entity[i] == "orange"])
+            gini = self.calculate_gini(num_apples / num_rows, num_oranges / num_rows)
+            scores[i] = gini
+            self.play(FadeOut(tree))
+
         self.wait(1)
-
-
-
-  
-
-
-
+        self.play(FadeOut(*self.mobjects))
 
         
 
+
 class e(Scene):
-    # scene to explain gini impurity
+    DEPTH = 3
+    CHILDREN_PER_VERTEX = 2
+    LAYOUT_CONFIG = {"vertex_spacing": (1.2, 1)}
+    VERTEX_CONF = {"radius": 0.25, "color": WHITE, "fill_opacity": 1}
+
+    def expand_vertex(self, g, vertex_id: str, depth: int):
+        new_vertices = [f"{vertex_id}/{i}" for i in range(self.CHILDREN_PER_VERTEX)]
+        new_edges = [(vertex_id, child_id) for child_id in new_vertices]
+        g.add_edges(
+            *new_edges,
+            vertex_config=self.VERTEX_CONF,
+            positions={k: g.vertices[vertex_id].get_center() + 0.1 * DOWN for k in new_vertices},
+        )
+        if depth < self.DEPTH:
+            for child_id in new_vertices:
+                self.expand_vertex(g, child_id, depth + 1)
+        return g
+
     def construct(self):
-        title = (Text("Gini Impurity", font="Montserrat", color=WHITE).scale(1.5))
-        title.to_edge(UP)
-        title.set_color_by_gradient(BLUE, GREEN, RED)
-        self.play(Write(title))
+        # Create and expand the tree
+        tree = Graph(["ROOT"], [], vertex_config=self.VERTEX_CONF)
+        tree = self.expand_vertex(tree, "ROOT", 1)
+        tree.scale(0.7)
+        self.play(
+            tree.animate.change_layout(
+                "tree",
+                root_vertex="ROOT",
+                layout_config=self.LAYOUT_CONFIG,
+            )
+        )
+
+
+        self.play((tree.vertices["ROOT"].animate.set_color(RED)))
         self.wait(1)
-
-        # draw a rectangle
-        rect = Rectangle(height=2, width=7, color=WHITE)
-        rect.set_color_by_gradient(BLUE, GREEN, RED)
-        rect.next_to(title, DOWN, buff=1.2)
-        self.play(Create(rect))
+        self.play(tree.vertices["ROOT/0"].animate.set_color(GREEN))
         self.wait(1)
-
-        # wirte gini formula in the rectangle
-        gini_formula = MathTex("Gini", "(", "t", ")", "=", "1", "-", "(", "p", "_", "0", "^2", "+", "p", "_", "1", "^2", ")")
-        gini_formula.scale(1.3)
-        gini_formula.next_to(title, DOWN, buff=1.9)
-        self.play(Write(gini_formula))
+        self.play(tree.vertices["ROOT/0/0"].animate.set_color(GREEN))
         self.wait(1)
-
-        gini_grp = VGroup(gini_formula, rect)
-
-        # scale the group down and move it to the left
-        self.play(gini_grp.animate.scale(0.7))  
-        self.play(gini_grp.animate.shift(LEFT*3))
-       
+        self.play(tree.vertices["ROOT/0/0/1"].animate.set_color(GREEN))
+        # reset the colors
+        self.play(
+            tree.vertices["ROOT"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0/0"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0/0/1"].animate.set_color(WHITE),
+        )
         self.wait(1)
+        # another way 
+        self.play((tree.vertices["ROOT"].animate.set_color(RED)))
+        self.wait(1)
+        self.play(tree.vertices["ROOT/0"].animate.set_color(GREEN))
+        self.wait(1)
+        self.play(tree.vertices["ROOT/0/0"].animate.set_color(GREEN))
+        self.wait(1)
+        self.play(tree.vertices["ROOT/0/0/0"].animate.set_color(GREEN))
+        # reset the colors
+        self.play(
+            tree.vertices["ROOT"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0/0"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0/0/0"].animate.set_color(WHITE),
+        )
 
- 
+        # another way 
+        self.play(tree.vertices["ROOT/0"].animate.set_color(GREEN))
+        self.wait(1)
+        self.play(tree.vertices["ROOT/0/1"].animate.set_color(GREEN))
+        self.wait(1)
+        self.play(tree.vertices["ROOT/0/1/0"].animate.set_color(GREEN))
+        # reset the colors
+        self.play(
+            tree.vertices["ROOT/0"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0/0"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0/1"].animate.set_color(WHITE),
+            tree.vertices["ROOT/0/1/0"].animate.set_color(WHITE),
+        )
+
+        self.wait(2)
+
+        self.play( tree.verticies["ROOT"].animate.set_color(RED))
+        self.wait(1)
+        self.play(tree.verticies["ROOT/1"].animate.set_color(GREEN))
+        self.wait(1)
+        self.play( tree.verticies["ROOT/1/0"].animate.set_color(GREEN))
+        self.wait(1)
+        self.play(tree.verticies["ROOT/1/0/1"].animate.set_color(GREEN))
+
+        self.wait(2)
+
+        # clean up
+        self.play(
+            tree.vertices["ROOT"].animate.set_color(WHITE),
+            tree.vertices["ROOT/1"].animate.set_color(WHITE),
+            tree.vertices["ROOT/1/0"].animate.set_color(WHITE),
+            tree.vertices["ROOT/1/0/1"].animate.set_color(WHITE),
+
+        )
+
+        self.play(FadeOut(*self.mobjects))
--- anim/Scene_a_outro.py
+++ anim/Scene_a_outro.py
@@ -1,11 +1,119 @@
 from manim import * 
-from retrieve import get_config
+import networkx as nx
 
 
 class Outro(Scene):
-    def __init__(self, **kwargs):
-        self.text = get_config()["outro"]
-        super().__init__(**kwargs)
+    def create_tree(self): 
+        G = nx.Graph()
+
+        G.add_node("ROOT")
+
+        for i in range(5):
+            G.add_node("Child_%i" % i)
+            G.add_node("Grandchild_%i" % i)
+            G.add_node("Greatgrandchild_%i" % i)
+
+            G.add_edge("ROOT", "Child_%i" % i)
+            G.add_edge("Child_%i" % i, "Grandchild_%i" % i)
+            G.add_edge("Grandchild_%i" % i, "Greatgrandchild_%i" % i)
+
+            # make all nodes black
+            G.nodes["Child_%i" % i]["color"] = "black"
+
+
+        return (Graph(list(G.nodes), list(G.edges), layout="tree", root_vertex="ROOT").set_color_by_gradient(BLUE, GREEN, RED))
+
+
     def construct(self):
-        text = Text(self.text, font="Montserrat", color=WHITE).scale(1.5)
-        self.play(Write(text))
+
+        title = Text("Zusammenfassung", font="Arial", color=WHITE).scale(1.5)
+        title.to_edge(UP)
+        self.play(Write(title))
+
+        # decision tree
+        decision_tree = self.create_tree()
+
+        decision_tree.scale(0.5)
+
+        decision_tree.shift(LEFT * 4)
+
+        tree_title = Text("Decision Trees", font="Arial", color=WHITE).scale(.5)
+
+        tree_title.next_to(decision_tree, DOWN, buff=0.5)
+
+        tree_sub_1 = Text("Fragen und Antwort basiert", font="Arial", color=WHITE).scale(.3)
+
+        tree_sub_1.next_to(tree_title, DOWN, buff=0.3)
+
+        tree_sub2 = Text("Baumstruktur", font="Arial", color=WHITE).scale(.3)
+
+        tree_sub2.next_to(tree_sub_1, DOWN, buff=0.3)
+
+
+
+        self.play(Write(decision_tree))
+
+        self.play(Write(tree_title), Create(decision_tree))
+
+        self.wait(2)
+
+        self.play(Write(tree_sub_1))
+
+        self.wait(2)
+
+        self.play(Write(tree_sub2))
+
+        self.wait(2)
+
+
+
+
+
+        # boosting
+        boosting_title = Text("Boosting", font="Arial", color=WHITE).scale(.5)
+        boosting_title.shift(UP)
+        bst_sub_1 = Text("Kombination von schwachen Lernalgorithmen", font="Arial", color=WHITE).scale(.3)
+        bst_sub_1.next_to(boosting_title, DOWN, buff=0.3)
+        bst_sub_2 = Text("zu einem starken Lernalgorithmus", font="Arial", color=WHITE).scale(.3)
+        bst_sub_2.next_to(bst_sub_1, DOWN, buff=0.3)
+        self.play(Write(boosting_title))
+        self.wait(2)
+        self.play(Write(bst_sub_1))
+        self.wait(1.3)
+        self.play(Write(bst_sub_2))
+        self.wait(2)
+
+        self.wait(2)
+
+
+        # random forest
+        random_forest = Text("Random Forests", font="Arial", color=WHITE).scale(.5)
+        random_forest.shift(RIGHT * 4.3)
+        random_forest.shift(UP * 1.5)
+
+        rfsubs0 = Text("Bagging", font="Arial", color=WHITE).scale(.5)
+        rfsubs0.next_to(random_forest, DOWN, buff=0.3)
+
+        rf_sub_1 = Text("Ensemble von Entscheidungsbäumen", font="Arial", color=WHITE).scale(.3)
+        rf_sub_1.next_to(rfsubs0, DOWN, buff=0.3)
+        rf_sub_2 = Text("Viele Bäume entscheiden", font="Arial", color=WHITE).scale(.3)
+        rf_sub_3 = Text("Mehrheitsabstimmung", font="Arial", color=WHITE).scale(.3)
+        rf_sub_2.next_to(rf_sub_1, DOWN, buff=0.3)
+        rf_sub_3.next_to(rf_sub_2, DOWN, buff=0.3)
+        
+        self.play(Write(random_forest))
+
+        self.wait(2)
+        self.play(Write(rfsubs0))
+        self.wait(2)
+        self.play(Write(rf_sub_1))      
+        self.wait(1.3)
+        self.play(Write(rf_sub_2))
+        self.wait(1.3)
+        self.play(Write(rf_sub_3))  
+        self.wait(4)
+        self.play(FadeOut(*self.mobjects)) # fade out all mobjects
+
+
+
+
